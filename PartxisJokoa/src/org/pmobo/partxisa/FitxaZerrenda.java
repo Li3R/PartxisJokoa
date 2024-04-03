479cb79c88e5312996bf3ff5076784465fb882d8
AsierArriaga <157583662+AsierArriaga@users.noreply.github.com>
2024-04-03 13:08:36 +0200

    Create FitxaZerrenda.java

---
+++ PartxisJokoa/src/org/pmobo/partxisa/FitxaZerrenda.java
@@ -0,0 +1,14 @@
+class FitxaZerrenda {
+    private ArrayList<Fitxa> fitxaZerrenda;
+
+    public FitxaZerrenda() {
+        fitxaZerrenda = new ArrayList<>();
+    }
+
+    // Implementación de métodos para gestionar la lista de fichas
+    // ...
+
+    public Iterator<Fitxa> getIterator() {
+        return fitxaZerrenda.iterator();
+    }
+}
