f3f3ded72ab2857932b66359ac6bdd5cebb2f165
AsierArriaga <157583662+AsierArriaga@users.noreply.github.com>
2024-04-03 13:09:50 +0200

    Create Tablero.java

---
+++ PartxisJokoa/src/org/pmobo/partxisa/Tablero.java
@@ -0,0 +1,41 @@
+class Tablero {
+    private static Tablero nireTablero = null;
+    private ArrayList<Casilla> casillas;
+
+    private Tablero() {
+        casillas = new ArrayList<Casilla>();
+    }
+
+    public static Tablero getTablero() {
+        if (nireTablero == null) {
+            nireTablero = new Tablero();
+        }
+        return nireTablero;
+    }
+
+    // Implementación de métodos para gestionar el tablero
+    // ...
+
+    public Iterator<Casilla> getIterator() {
+        return casillas.iterator();
+    }
+
+    public void printCasillas() {
+        Iterator<Casilla> iterator = casillas.iterator();
+        int position = 0;
+        while (iterator.hasNext()) {
+            Casilla casilla = iterator.next();
+            System.out.print("Position: " + position + ", Type: " + casilla.getClass().getSimpleName());
+            if (casilla instanceof Casita) {
+                System.out.println(", Casita");
+            } else if (casilla instanceof Inicio) {
+                System.out.println(", Inicio");
+            } else if (casilla instanceof Final) {
+                System.out.println(", Final");
+            } else {
+                System.out.println(", Normal");
+            }
+            position++;
+        }
+    }
+}
