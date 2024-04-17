4696213216ba7f9799cc69e9e1b8d48c6b5a999f
Li3R <llaiseka001@ikasle.ehu.eus>
2024-04-17 10:02:57 +0200

    tableroaSortu

--- PartxisJokoa/src/org/pmobo/partxisa/Tablero.java
+++ PartxisJokoa/src/org/pmobo/partxisa/Tablero.java
@@ -18,28 +18,18 @@ class Tablero {
     }
 
     public void tableroaSortu() {
-        for (int i = 0; i < 68; i++) {
-            kasillaZerrenda.add(new Kasilla(false, i));
+        Iterator<Kasilla> itr = getIterator();
+        while (itr.hasNext() && kasillaZerrenda.size() < 68){
+            Kasilla kasilla = itr.next();
+            // Do something with each Kasilla object
         }
     }
+
     public void tabletoaInprimatu() {
         Iterator<Kasilla> itr = getIterator();
         while (itr.hasNext()) {
             Kasilla kasilla = itr.next();
-            System.out.println("Kasilla zenbakia: " + kasilla.kasillaPos);
-            if (kasilla.fitxaDago) {
-                System.out.println("Kasilla honetan fitxa bat dago");
-            }
-            if (kasilla instanceof KasillaNormala) {
-                System.out.println("Kasilla normala");
-            } else if (kasilla instanceof KasillaFinala) {
-                System.out.println("Kasilla Finala");
-            } else if (kasilla instanceof BabesKasilla) {
-                System.out.println("Babes kasilla");
-            } else if (kasilla instanceof EtxekoKasilla) {
-                System.out.println("Etxeko kasilla");
-            }
-        
+            System.out.println("Kasilla zenbakia: " + kasilla.kasillaPos + "Fitxarik dago? " + kasilla.fitxaDago);
         }
     }
 }
