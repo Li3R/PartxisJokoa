bc72690fb764ce7ddb6b5c2b469dcd4bc9693053
Lier <131645790+Li3R@users.noreply.github.com>
2024-04-22 21:13:57 +0200

    Update Kasilla.java

--- PartxisJokoa/src/org/pmobo/partxisa/Kasilla.java
+++ PartxisJokoa/src/org/pmobo/partxisa/Kasilla.java
@@ -2,13 +2,11 @@ package org.pmobo.partxisa;
 
 public class Kasilla {
     private int numKasilla;
-    private boolean okupatuta;
     private Fitxa fitxa;
 
-    public Kasilla(int numKasilla, boolean okupatuta, Fitxa fitxa) {
-        this.numKasilla = numKasilla;
-        this.okupatuta = okupatuta;
-        this.fitxa = fitxa;
+    public Kasilla(int pNumKasilla, Fitxa pFitxa) {
+        this.numKasilla = pNumKasilla;
+        this.fitxa = pFitxa;
     }
 
     public int getNumKasilla() {
@@ -16,20 +14,6 @@ public class Kasilla {
     }
 
     public boolean okupatutaDago() {
-        return this.okupatuta;
-    }
-
-    public void ponerFitxa(Fitxa fitxa) {
-        this.fitxa = fitxa;
-        this.okupatuta = true;
-    }
-
-    public Fitxa obtenerFitxa() {
-        return this.fitxa;
-    }
-
-    public void vaciarCasilla() {
-        this.fitxa = null;
-        this.okupatuta = false;
+        return this.numKasilla == this.fitxa.fitxarenPosizioa();
     }
 }
