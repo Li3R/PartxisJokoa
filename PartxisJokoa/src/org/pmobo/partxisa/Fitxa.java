a02d99c3d0e14d2cd093d0a21add5cbba8ef175b
Lier <131645790+Li3R@users.noreply.github.com>
2024-04-24 19:44:43 +0200

    Update Fitxa.java

--- PartxisJokoa/src/org/pmobo/partxisa/Fitxa.java
+++ PartxisJokoa/src/org/pmobo/partxisa/Fitxa.java
@@ -3,11 +3,15 @@ package org.pmobo.partxisa;
 public class Fitxa {
 	private int pos;
 	
-	public Fitxa(int pPos) {
-		this.pos = pPos;
+	public Fitxa() {
+		this.pos = 0;
 	}
 	
 	public int fitxarenPosizioa() {
 		return this.pos;
+	}
+	
+	public void setFitxarenPosizioa(int pDado) {
+		this.pos = this.pos + pDado;		
 	}
 }
